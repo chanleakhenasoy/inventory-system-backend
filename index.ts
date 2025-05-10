214704e9f6b6ece68aba43cf42b9069d3ddafc79
chanleakhenasoy <soychanleakhena@gmail.com>
2025-05-10 05:44:41 -0400

    create-product

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import  authRouter  from './src/routes/auth.route';
 import supplierRouter from './src/routes/supplier.route';
 import categoryRouter from './src/routes/category.route';
+import productRouter from './src/routes/product.route';
 
 
 const app = express();
@@ -12,7 +13,7 @@ app.use(express.json());
 app.use("/api/auth", authRouter);
 app.use("/api/supplier",supplierRouter);
 app.use("/api/category", categoryRouter);
-// app.use("/api/product", productRouter);
+app.use("/api/product", productRouter);
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
--- src/config/querySql/table.sql
+++ src/config/querySql/table.sql
@@ -23,3 +23,19 @@ CREATE TABLE categories (
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP 
 );
+
+CREATE TABLE products (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  category_id UUID NOT NULL,
+  product_code VARCHAR(50) NOT NULL UNIQUE,
+  name_en VARCHAR(255) NOT NULL,
+  name_kh VARCHAR(255) NOT NULL,
+  beginning_quantity INTEGER NOT NULL DEFAULT 0,
+  created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+  updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+  CONSTRAINT fk_category
+    FOREIGN KEY (category_id) 
+    REFERENCES categories(id) 
+    ON DELETE CASCADE
+);
+
